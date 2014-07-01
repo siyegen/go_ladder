d88d1b063c4d7e8c4663260af8f6539fee4dd595
David Tomberlin <siyegen@gmail.com>
2014-07-01 00:20:46 -0700

    trying to add chan handling and syncing

--- src/go_ladder/main.go
+++ src/go_ladder/main.go
@@ -5,25 +5,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
+
+type WordResult struct {
+	size     int
+	wordChan chan string
+}
 
 func main() {
 	fmt.Println("Hi Words")
 
 	chanMap := make(map[int]chan string)
-	metaChan := make(chan chan string)
+	metaChan := make(chan WordResult)
+	doneChan := make(chan struct{})
+	var wg sync.WaitGroup
 
 	go func() {
 		for {
 			dd := <-metaChan
-			go func(wordSizedChan chan string) {
-				count := 0
-				for word := range wordSizedChan {
-					fmt.Println(len(word))
-					count++
-				}
-				fmt.Printf("Size %d words: %d", 2, count)
-			}(dd)
+			go handleWord(dd.size, doneChan, dd.wordChan, wg)
 		}
 	}()
 
@@ -39,16 +40,31 @@ func main() {
 		if !ok {
 			sizedChan = make(chan string)
 			chanMap[len(word)] = sizedChan
-			metaChan <- sizedChan
+			metaChan <- WordResult{len(word), sizedChan}
 			fmt.Println("New Chan", len(word))
+			wg.Add(1)
 		}
 		// Add wg here, remove it on processing side?
 		sizedChan <- word
 	}
-	close(metaChan)
+	close(doneChan)
+	wg.Wait()
+	// close(metaChan)
 	// We could range over all channels and close them?
 }
 
-func handleWord() {
-
+func handleWord(size int, done <-chan struct{}, wordSizedChan chan string, wg sync.WaitGroup) {
+	count := 0
+	for word := range wordSizedChan {
+		select {
+		case <-done:
+			close(wordSizedChan)
+			log.Println("Closing wordSizedChan")
+		default:
+			fmt.Println(word, size)
+			count++
+		}
+	}
+	fmt.Printf("Size %d words: %d\n", size, count)
+	wg.Done()
 }
