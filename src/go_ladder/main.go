41356a5a5f57869bd6dd2777fc44e829ec023cc4
David Tomberlin <siyegen@gmail.com>
2014-07-03 23:29:28 -0700

    remove nasty label break with channels

--- src/go_ladder/main.go
+++ src/go_ladder/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"sync"
+	"time"
 )
 
 type WordResult struct {
@@ -18,59 +19,70 @@ func main() {
 
 	chanMap := make(map[int]chan string)
 	metaChan := make(chan WordResult)
-	doneChan := make(chan struct{})
-	var wg sync.WaitGroup
 
-	go func() {
-		for {
-			dd := <-metaChan
-			go handleWord(dd.size, doneChan, dd.wordChan, &wg)
-		}
-	}()
+	finishedChan := handleSizedChan(metaChan)
 
 	file, err := os.Open("./words")
 	if err != nil {
 		log.Fatal("Can't open file", err)
 	}
+
 	scanner := bufio.NewScanner(file)
+	// Read line by line, send each word on its sized channel
 	for scanner.Scan() {
 		var sizedChan chan string
 		word := scanner.Text()
+		// Try to get channel for n size from map, otherwise create
 		sizedChan, ok := chanMap[len(word)]
 		if !ok {
 			sizedChan = make(chan string)
 			chanMap[len(word)] = sizedChan
+			// On creation we need to setup the chan to get data
 			metaChan <- WordResult{len(word), sizedChan}
 			fmt.Println("New Chan", len(word))
-			wg.Add(1)
 		}
-		// Add wg here, remove it on processing side?
+		// Send the word over, our work is finished
 		sizedChan <- word
 	}
-	close(doneChan)
-	for size, ch := range chanMap {
-		fmt.Println("Closing channel for", size)
+	// Wait for shit to finish, maybe select with kill later
+	time.Sleep(3 * time.Second)
+	for _, ch := range chanMap {
 		close(ch)
 	}
-	// wg.Wait()
-	// close(metaChan)
-	// We could range over all channels and close them?
+	fmt.Println("Waiting for shit to finish")
+	<-finishedChan
 }
 
-func handleWord(size int, done <-chan struct{}, wordSizedChan chan string, wg *sync.WaitGroup) {
+func handleWord(size int, wordSizedChan chan string) {
 	count := 0
-LOOP:
-	for {
-		select {
-		case <-done:
-			close(wordSizedChan)
-			log.Println("Closing wordSizedChan")
-			break LOOP
-		case word := <-wordSizedChan:
-			fmt.Println(word, size)
-			count++
-		}
+	for word := range wordSizedChan {
+		fmt.Println("handleWord", word)
+		count++
 	}
 	fmt.Printf("Size %d words: %d\n", size, count)
-	// wg.Done()
+}
+
+func handleSizedChan(metaChan chan WordResult) chan struct{} {
+	finished := make(chan struct{})
+	var wg sync.WaitGroup
+	go func() {
+		for {
+			dd := <-metaChan
+			fmt.Println("adding wg")
+			wg.Add(1)
+			go func() {
+				handleWord(dd.size, dd.wordChan)
+				fmt.Println("wg.done")
+				wg.Done()
+			}()
+		}
+	}()
+
+	go func() {
+		wg.Wait()
+		fmt.Println("time to close chan")
+		close(finished)
+	}()
+
+	return finished
 }
